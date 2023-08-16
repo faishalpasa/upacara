1a0ac3642a2d31649e10a4be77259dc8691a9dc2
Faishal Pasa <faishal.pasa@bareksa.com>
2023-08-16 21:22:57 +0700

    feat: game

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const charactersElement = document.getElementById('characters');
 const chatBubbleElement = document.getElementById('chat-bubble');
 const flagElement = document.getElementById('flag');
 const flagWaveElement = document.getElementById('flagwave');
-const buttonWrapper = document.getElementById('button-wrapper');
+const buttonWrapperElement = document.getElementById('button-wrapper');
+const actionButtonWrapperElement = document.getElementById('action-button-wrapper');
+const pointWrapperElement = document.getElementById('point');
+const totalPointWrapperElement = document.getElementById('total-point');
+const backgroundElement = document.getElementById('background');
+const overlayElement = document.getElementById('overlay');
 const audio = document.getElementById('audio');
+
+let point = 0;
+let seconds = 0;
+let click = 0;
+pointWrapperElement.innerText = click;
+let audioDuration = 0
+let durationInterval
 
 const handlePlayAudio = () => {
   audio.play();
@@ -12,24 +24,39 @@ const handlePlayAudio = () => {
 
 const handlePlayCeremony = () => {
   handlePlayAudio()
-  const audioDuration = Math.floor(audio.duration)
 
   chatBubbleElement.style.opacity = '0';
   charactersElement.style.transition = `opacity 0.5s`;
-  
-  let seconds = 0;
-  const percent = 100 / audioDuration;
 
-  this.setInterval(() => {
-    flagElement.style.bottom = `calc(100% - 200px)`;
-    flagElement.style.transition = `bottom ${audioDuration}s`;
-    seconds += 1;
+  actionButtonWrapperElement.style.bottom = '16px';
+  actionButtonWrapperElement.style.transition = `bottom 1s`;
+
+  pointWrapperElement.style.top = '16px';
+  pointWrapperElement.style.transition = `top 1s`;
+
+  durationInterval = setInterval(() => {
+    handleCountDuration()
   }, [1000])
+
+  audioDuration = Math.floor(audio.duration)
+}
+
+const handleCountDuration = () => {
+  seconds += 1;
+
+  if (seconds > audioDuration) {
+    clearInterval(durationInterval)
+    actionButtonWrapperElement.style.bottom = '-116px';
+    actionButtonWrapperElement.style.transition = `bottom 1s`;
+    pointWrapperElement.style.top = '-116px';
+    pointWrapperElement.style.transition = `top 1s`;
+    overlayElement.style.display = 'block';
+  }
 }
 
 const handleClickStartButton = () => {
-  buttonWrapper.style.bottom = '-10%';
-  buttonWrapper.style.transition = `bottom 1s`;
+  buttonWrapperElement.style.bottom = '-10%';
+  buttonWrapperElement.style.transition = `bottom 1s`;
 
   flagPoleElement.style.right = '0%';
   flagPoleElement.style.transition = `right 1s`;
@@ -45,6 +72,29 @@ const handleClickStartButton = () => {
   setTimeout(() => {
     handlePlayCeremony();
   }, 4000)
+}
 
+const handleClickActionButton = () => {
+  let clickPoint = 0;
+  if (click < 100 && seconds < audioDuration) {
+    click += 1;
+    clickPoint = click + seconds;
+    point = point + clickPoint;
+    pointWrapperElement.innerText = point;
+    totalPointWrapperElement.innerText = point;
+    flagElement.style.bottom = `calc(${click}% - 200px)`;
+    flagElement.style.transition = `bottom 0.5s`;
+  }
+
+  const clickPointElement = document.createElement('div');
+  clickPointElement.classList.add('click-point');
+  clickPointElement.innerText = `+${clickPoint}`;
+
+  backgroundElement.appendChild(clickPointElement);
+  setTimeout(() => {
+    clickPointElement.style.bottom = '140px';
+    clickPointElement.style.transition = `bottom 0.9s`;
+  }, 100);
+  setTimeout(() => clickPointElement.remove(), 1000);
 }
 
